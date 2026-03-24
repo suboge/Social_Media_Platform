e567a0143d854eb7f63538dcc3d103df1a2d813b
suboge <elliotccleoleo@gmail.com>
2026-03-24 20:05:50 +0800

    yml setting

---
+++ src/main/java/com/popo/e_sum_social_media_platform/ESumSocialMediaPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.popo.e_sum_social_media_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESumSocialMediaPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESumSocialMediaPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/popo/e_sum_social_media_platform/ESumSocialMediaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popo.e_sum_social_media_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESumSocialMediaPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
